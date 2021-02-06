9dd746e2bb7c035191df2c8eff542704458b918b
Andrei Dobre <andreidobre.web@gmail.com>
2021-02-06 19:15:09 +0200

    add argocd box

---
+++ kind-boxes/argocd/README.md
@@ -0,0 +1,6 @@
+### ArgoCD experiments
+
+kind setup for trying out Argo CD.
+
+Install:
+
---
+++ kind-boxes/argocd/makebox.sh
@@ -0,0 +1,2 @@
+DEFAULT_INSTALL_PATH="$(git rev-parse --show-toplevel)/kind-cluster/generator/install.sh"
+source $DEFAULT_INSTALL_PATH
